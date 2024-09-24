1d9bd9b48b21e721e46eb731138276e1bfeb01a7
Zack Rowell <zjrowell@crimson.ua.edu>
2024-09-23 20:09:44 -0500

    adding project to repo

---
+++ Fall2024-Assn1-zjrowell/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Fall2024_Assn1_zjrowell.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
